483cfc54e88a073446a848830bba38026fcb368a
JJ Wang <jonathanjacobwang@gmail.com>
2024-08-13 15:17:30 -0700

    tailwind not working properly

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'           
+import App from './App.jsx'     
+import './index.css'; 
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
